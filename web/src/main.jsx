3da836de55ba161d808f0002f3df5866632c1ee5
Diego <diegobcuadros@gmail.com>
2025-08-01 13:02:47 -0400

    Relocate App/index.jsx for readability

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "@/assets/index.css";
-import App from "./App.jsx";
+import App from "@/App";
 
 createRoot(document.getElementById("root")).render(<App />);
