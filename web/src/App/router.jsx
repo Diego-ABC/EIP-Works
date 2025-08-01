6bd97e832779980789e6c85ea13d0f7086491363
Diego <diegobcuadros@gmail.com>
2025-08-01 15:35:40 -0400

    Add signin and signout function to router

--- web/src/App/router.jsx
+++ web/src/App/router.jsx
@@ -3,12 +3,20 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router";
+import SignIn from "./features/auth/pages/SignIn";
+import signOutLoader from "./features/auth/services/signOutLoader";
+import signInAction from "./features/auth/services/signInAction";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<>main dashboard</>}></Route>
-      <Route path="/signin" element={<>sign in page</>} />
+      <Route
+        path="/signin"
+        element={<SignIn />}
+        loader={signOutLoader}
+        action={signInAction}
+      />
     </Route>
   )
 );
