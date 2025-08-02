ba203ce6bef57cca3bdd34ea0aa725d7de055657
Diego <diegobcuadros@gmail.com>
2025-08-02 16:52:54 -0400

    Add basic new staff form page

---
+++ web/src/App/features/staff/pages/NewStaff.jsx
@@ -0,0 +1,72 @@
+import TextInputField from "@/components/formComponents/TextInputField";
+import StaffListHeader from "../components/StaffListHeader";
+import { Form, useActionData } from "react-router";
+
+export default function NewStaff() {
+  const { error } = useActionData() || {};
+  return (
+    <>
+      <StaffListHeader>Staff {">"} New</StaffListHeader>
+      <Form className="w-fit grid grid-cols-4 gap-2 p-5 bg-base-100">
+        <TextInputField
+          name="firstName"
+          placeholder="First Name"
+          className="col-span-2 w-full"
+        />
+        <TextInputField
+          name="lastName"
+          placeholder="Last Name"
+          className="col-span-2 w-full"
+        />
+        <TextInputField
+          name="email"
+          placeholder="email"
+          className="col-span-4 w-full"
+        />
+        <TextInputField
+          name="cell"
+          placeholder="mobile number"
+          className="col-span-4 w-full"
+        />
+        <TextInputField
+          name="work"
+          placeholder="work number"
+          className="col-span-3 w-full"
+        />
+        <TextInputField
+          name="workExt"
+          placeholder="ext"
+          className="col-span-1 min-w-0 w-36"
+        />
+        <TextInputField
+          type="date"
+          name="dateJoined"
+          label="start date"
+          className="col-span-2"
+        />
+        <div className="px-4 rounded-lg bg-base-200 col-span-2 flex flex-row items-center">
+          <label className="flex flex-row items-center gap-2">
+            <input
+              type="checkbox"
+              className="checkbox checked:checkbox-success peer"
+              name="sendInvite"
+            />
+            <span className="font-medium peer-checked:text-success transition-colors cursor-pointer">
+              Send Invite?
+            </span>
+          </label>
+        </div>
+
+        {/* <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-64 border p-4">
+          <legend className="fieldset-legend">Send Invite</legend>
+          <label className="label">
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </fieldset> */}
+        <button className="btn btn-soft dark:btn btn-success btn-lg col-span-4">
+          Save
+        </button>
+      </Form>
+    </>
+  );
+}
