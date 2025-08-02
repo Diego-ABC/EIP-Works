5241b0022f1ba2e7a8a90c53f66f3102c2aaae41
Diego <diegobcuadros@gmail.com>
2025-08-01 20:14:05 -0400

    Create DarkModeToggle button

---
+++ web/src/App/layout/DashboardLayout/DarkModeToggle.jsx
@@ -0,0 +1,20 @@
+import Moon from "@/assets/icons/Moon";
+import Sun from "@/assets/icons/Sun";
+import getDarkMode from "@/lib/utils/darkmode/getDarkMode";
+import setDarkMode from "@/lib/utils/darkmode/setDarkMode";
+import { useEffect, useState } from "react";
+
+export default function DarkModeToggle() {
+  const [isDarkMode, setIsDarkMode] = useState(getDarkMode());
+  useEffect(() => {
+    setDarkMode(isDarkMode);
+  }, [isDarkMode]);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  return (
+    <button onClick={toggleDarkMode} className={"btn btn-accent"}>
+      {isDarkMode ? <Sun /> : <Moon />}
+    </button>
+  );
+}
