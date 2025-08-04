11128b69166c17e94f0e9e4c86feaf7ab43aceda
Diego <diegobcuadros@gmail.com>
2025-08-04 14:05:28 -0400

    Update signin form page with consistent TextInputField component

--- web/src/App/features/auth/pages/SignIn.jsx
+++ web/src/App/features/auth/pages/SignIn.jsx
@@ -1,3 +1,4 @@
+import TextInputField from "@/components/formComponents/TextInputField";
 import { Form, Link, useActionData } from "react-router";
 
 export default function SignIn() {
@@ -12,27 +13,33 @@ export default function SignIn() {
         <div className="card-title border-b-2 border-dashed border-b-base-200 bg-base-100 p-4 text-2xl justify-center font-medium">
           Sign in
         </div>
-        <div className="card-body gap-0">
-          <input
+        <div className="card-body gap-5">
+          {/* <input
             type="text"
             name="email"
             className="mb-5 input input-lg w-full focus:outline-none focus:border-b-accent border-0 border-b-2 transition-colors duration-200 bg-base-300/20 focus:bg-transparent font-medium"
             placeholder="email"
+          /> */}
+          <TextInputField name="email" placeholder="email" className="w-full" />
+          <TextInputField
+            name="password"
+            placeholder="password"
+            className="w-full"
           />
-          <input
+          {/* <input
             type="password"
             name="password"
             className="mb-2 input input-lg w-full focus:outline-none focus:border-b-accent border-0 border-b-2 transition-colors duration-200 bg-base-300/20 focus:bg-transparent font-medium"
             placeholder="password"
-          />
-          <div className="mb-2 w-full flex flex-row justify-end">
+          /> */}
+          {/* <div className="mb-2 w-full flex flex-row justify-end">
             <Link
               to="/forgot"
               className=" link link-hover link-accent py-2 px-4 text-right"
             >
               Forgot Password?
             </Link>
-          </div>
+          </div> */}
           <button type="submit" className="btn btn-lg btn-primary">
             Submit
           </button>
