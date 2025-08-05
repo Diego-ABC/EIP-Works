ac51da43a1bad12f35b225e66df5c311520f7c0f
Diego <diegobcuadros@gmail.com>
2025-08-05 19:46:24 -0400

    Add CaseList implementation of GridTable

---
+++ web/src/App/features/cases/components/CasesListTable.jsx
@@ -0,0 +1,20 @@
+// function CaseListRow(){
+//   return
+// }
+
+import GridTable from "@/components/dataDisplay/GridTable/GridTable";
+
+export default function CasesListTable({
+  headers = [],
+  cases = [],
+  className,
+}) {
+  // const headerData = ["test1", "test2"];
+  // const row1 = ["td1", "td2"];
+  console.log(cases);
+  return (
+    <GridTable cols={headers.length} className="w-fit">
+      {[headers, ...cases]}
+    </GridTable>
+  );
+}
