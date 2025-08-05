48bffbddb067588ff5d27686f3833f17414d4365
Diego <diegobcuadros@gmail.com>
2025-08-05 19:44:57 -0400

    Add bottom margin to dashboard page header

--- web/src/App/layout/DashboardLayout/DashboardPage/DashboardHeader.jsx
+++ web/src/App/layout/DashboardLayout/DashboardPage/DashboardHeader.jsx
@@ -1,7 +1,9 @@
 export default function DashboardHeader({ children, className }) {
   return (
     <h1
-      className={"text-3xl font-semibold" + (className ? " " + className : "")}
+      className={
+        "text-3xl font-semibold mb-5" + (className ? " " + className : "")
+      }
     >
       {children}
     </h1>
