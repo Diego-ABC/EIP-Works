b56cee693ce9d3b89f5c4a136e499a0c5e31f7ab
Diego <diegobcuadros@gmail.com>
2025-08-05 21:59:42 -0400

    Add option for right side label

--- web/src/components/formComponents/TextInputField.jsx
+++ web/src/components/formComponents/TextInputField.jsx
@@ -5,18 +5,20 @@ export default function TextInputField({
   name,
   placeholder,
   className = "",
+  labelRight = false,
 }) {
   // if (label) return <label></label>
 
   return (
     <label className={textFieldStyle + " " + className}>
-      {label && <span className="label">{label}</span>}
+      {label && !labelRight && <span className="label">{label}</span>}
       <input
         type={type}
         name={name}
         className="outline-0"
         placeholder={placeholder}
       />
+      {label && labelRight && <span className="label">{label}</span>}
     </label>
   );
 }
