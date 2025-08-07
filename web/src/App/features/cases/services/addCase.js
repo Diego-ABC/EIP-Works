61de050f438ed54bf0624544daa7f0b43ca64622
Diego <diegobcuadros@gmail.com>
2025-08-07 09:33:22 -0400

    Add missing imports for addCase.js

--- web/src/App/features/cases/services/addCase.js
+++ web/src/App/features/cases/services/addCase.js
@@ -1,4 +1,4 @@
-import { doc, getFirestore } from "firebase/firestore";
+import { doc, getFirestore, collection, setDoc } from "firebase/firestore";
 
 export default async function addCase(caseData) {
   const newCaseRef = doc(collection(getFirestore(), "cases"));
