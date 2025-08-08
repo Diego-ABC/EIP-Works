680bc9ca352e547adb00d609450fb463e0f05302
Diego <diegobcuadros@gmail.com>
2025-08-07 22:54:28 -0400

    Add css to let active link pop out

--- web/src/App/layout/TabLinks/index.jsx
+++ web/src/App/layout/TabLinks/index.jsx
@@ -5,14 +5,16 @@ export default function TabLinks({ className, links = [] }) {
   return (
     <div className={classMerge(className, "flex flex-row")}>
       <div className="w-4 border-b-2 border-b-black dark:border-b-white"></div>
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-3 items-end">
         {links.map(({ href, name }) => (
           <NavLink
             key={href}
             to={href}
             className={({ isActive }) =>
-              "border-black dark:border-white border-2 justify-center flex flex-row px-4 py-2 font-medium not-first:border-l-0" +
-              (isActive ? " border-b-0" : " bg-primary/20 dark:bg-primary")
+              "grow border-black dark:border-white first:border-l-2 border-t-2 justify-center flex flex-row px-4 font-medium group-first:border-l-2" +
+              (isActive
+                ? " border-b-0 pb-3 pt-3 border-l-2 border-r-2"
+                : " bg-primary/20 dark:bg-primary py-2 border-b-2 border-r-2 [&:has(+*.pt-3)]:border-r-0")
             }
           >
             {name}
