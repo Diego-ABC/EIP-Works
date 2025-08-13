90e30a40dc1acfe15d545486b9ff0d957d82ce42
Diego <diegobcuadros@gmail.com>
2025-08-13 13:47:42 -0400

    Switch tablinks to use flexbox instead of grid

--- web/src/App/layout/TabLinks/index.jsx
+++ web/src/App/layout/TabLinks/index.jsx
@@ -1,17 +1,39 @@
 import classMerge from "@/lib/utils/stylings/classMerge";
 import { NavLink } from "react-router";
 
+const colCountClass = [
+  "grid-cols-1",
+  "grid-cols-2",
+  "grid-cols-3",
+  "grid-cols-4",
+  "grid-cols-5",
+  "grid-cols-6",
+  "grid-cols-7",
+  "grid-cols-8",
+  "grid-cols-9",
+  "grid-cols-10",
+];
+
+const Padding = ({ grow = false }) => (
+  <div
+    className={
+      "w-4 border-b-2 border-b-black dark:border-b-white" +
+      (grow ? " grow" : "")
+    }
+  ></div>
+);
+
 export default function TabLinks({ className, links = [] }) {
   return (
     <div className={classMerge(className, "flex flex-row")}>
-      <div className="w-4 border-b-2 border-b-black dark:border-b-white"></div>
-      <div className="grid grid-cols-3 items-end">
+      <Padding />
+      <div className="flex flex-row items-end">
         {links.map(({ href, name }) => (
           <NavLink
             key={href}
             to={href}
             className={({ isActive }) =>
-              "grow border-black dark:border-white first:border-l-2 border-t-2 justify-center flex flex-row px-4 font-medium group-first:border-l-2 w-36" +
+              "grow border-black dark:border-white first:border-l-2 border-t-2 justify-center flex flex-row px-4 font-medium group-first:border-l-2 " +
               (isActive
                 ? " border-b-0 pb-3 pt-3 border-l-2 border-r-2"
                 : " bg-primary/20 dark:bg-primary py-2 border-b-2 border-r-2 [&:has(+*.pt-3)]:border-r-0")
@@ -21,7 +43,29 @@ export default function TabLinks({ className, links = [] }) {
           </NavLink>
         ))}
       </div>
-      <div className="grow border-b-2 border-b-black dark:border-b-white"></div>
+      <Padding grow />
     </div>
   );
+  // return (
+  //   <div className={classMerge(className, "flex flex-row")}>
+  //     <div className="w-4 border-b-2 border-b-black dark:border-b-white"></div>
+  //     <div className={"grid items-end auto-cols-auto"}>
+  //       {links.map(({ href, name }) => (
+  //         <NavLink
+  //           key={href}
+  //           to={href}
+  //           className={({ isActive }) =>
+  //             "grow border-black dark:border-white first:border-l-2 border-t-2 justify-center flex flex-row px-4 font-medium group-first:border-l-2 w-36" +
+  //             (isActive
+  //               ? " border-b-0 pb-3 pt-3 border-l-2 border-r-2"
+  //               : " bg-primary/20 dark:bg-primary py-2 border-b-2 border-r-2 [&:has(+*.pt-3)]:border-r-0")
+  //           }
+  //         >
+  //           {name}
+  //         </NavLink>
+  //       ))}
+  //     </div>
+  //     <div className="grow border-b-2 border-b-black dark:border-b-white"></div>
+  //   </div>
+  // );
 }
