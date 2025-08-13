ad1e101b38b862708c77c9250fbf2a7c1675d848
Diego <diegobcuadros@gmail.com>
2025-08-12 23:10:15 -0400

    Add some layout changes to GridTableCell css

--- web/src/components/dataDisplay/GridTable/GridTableCell.jsx
+++ web/src/components/dataDisplay/GridTable/GridTableCell.jsx
@@ -1,7 +1,7 @@
 import classMerge from "@/lib/utils/stylings/classMerge";
 
 const cellClass =
-  "border-b-2 border-r-2 border-black dark:border-white font-medium px-10 py-5 group-hover:bg-black/20 group-hover:dark:bg-white/20";
+  "flex flex-row items-center border-b-2 border-r-2 border-black dark:border-white font-medium px-6 py-3 group-hover:bg-black/20 group-hover:dark:bg-white/20";
 
 export default function GridTableCell({ children, className = "" }) {
   return <div className={classMerge(className, cellClass)}>{children}</div>;
