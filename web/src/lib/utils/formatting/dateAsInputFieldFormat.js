e695785369c668c746b9b940211777f6d0618a71
Diego <diegobcuadros@gmail.com>
2025-08-12 23:08:23 -0400

    Add Date to string format helper function

---
+++ web/src/lib/utils/formatting/dateAsInputFieldFormat.js
@@ -0,0 +1,8 @@
+export default function dateAsInputFieldFormat(date = new Date()) {
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  if (month < 10) month = "0" + month;
+  let day = date.getDate();
+  if (day < 10) day = "0" + day;
+  return `${year}-${month}-${day}`;
+}
