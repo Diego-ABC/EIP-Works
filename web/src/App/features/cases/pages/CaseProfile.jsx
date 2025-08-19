c1867f41d8024b5de1171e19445c96cdd095eac9
Diego <diegobcuadros@gmail.com>
2025-08-19 11:25:21 -0400

    Update case profile display to prefilled form display

--- web/src/App/features/cases/pages/CaseProfile.jsx
+++ web/src/App/features/cases/pages/CaseProfile.jsx
@@ -1,20 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useCase } from "../context/caseContext";
+import CaseProfileForm from "../components/CaseProfileForm";
+import { useState } from "react";
+import { useActionData } from "react-router";
 
 export default function CaseProfile() {
   const { eiCase } = useCase();
+  const [formEdited, setFormEdited] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+
+  const actionData = useActionData();
+  const { success, error } = actionData || {};
+
+  useEffect(() => {
+    setSubmitting(false);
+    setFormEdited(false);
+  }, [actionData]);
   return (
-    <div className="grid grid-cols-[auto_auto] w-fit p-3">
-      {Object.entries(eiCase).map(([id, value]) => (
-        <Fragment key={id}>
-          <div className="border-b-2 border-b-black dark:border-b-white pr-5">
-            {id}:
-          </div>
-          <div className="border-b-2 border-b-black dark:border-b-white">
-            {value}
-          </div>
-        </Fragment>
-      ))}
+    <div className="@container w-full">
+      <CaseProfileForm
+        patient={eiCase}
+        canSave={formEdited}
+        onChange={() => {
+          setFormEdited(true);
+        }}
+        onSubmit={() => {
+          setSubmitting(true);
+        }}
+        submitting={submitting}
+      />
     </div>
   );
 }
