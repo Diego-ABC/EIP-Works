44c410c1ee5f00bcd7c7ab75514a5d1aba6311c4
Diego <diegobcuadros@gmail.com>
2025-08-19 11:11:23 -0400

    Modify button style to be more boxy

--- web/src/components/buttons/AddLinkButton.jsx
+++ web/src/components/buttons/AddLinkButton.jsx
@@ -3,7 +3,10 @@ import { PlusIcon } from "@phosphor-icons/react";
 
 export default function AddLinkbutton({ href }) {
   return (
-    <Link to={href} className="btn dark:btn-soft btn-success font-bold mb-5">
+    <Link
+      to={href}
+      className="btn dark:btn-soft btn-outline btn-success font-bold mb-5"
+    >
       Add
       <PlusIcon className="size-4" weight="bold" />
     </Link>
