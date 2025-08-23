9815f490490a7de06fbeb74b7faf56a275adce9f
Diego <diegobcuadros@gmail.com>
2025-08-22 21:01:32 -0400

    Add staffing and feedback navlinks to navpane

--- web/src/App/layout/DashboardLayout/NavPane.jsx
+++ web/src/App/layout/DashboardLayout/NavPane.jsx
@@ -3,11 +3,17 @@ import DarkModeToggle from "./DarkModeToggle";
 import {
   AddressBookTabsIcon,
   BabyIcon,
+  // CalendarIcon,
+  CircuitryIcon,
   FinnTheHumanIcon,
   IdentificationCardIcon,
 } from "@phosphor-icons/react";
 import { Link } from "react-router";
 import NavLink from "./NavLink";
+import FeatureRequestNavLink from "@/App/features/feedback/components/FeedbackPageNavlink";
+// import DatePicker from "@/components/formComponents/DatePicker";
+// import TextInputField from "@/components/formComponents/TextInputField";
+// import textFieldStyle from "@/components/formComponents/textFieldStyle";
 
 const rootRoutes = [
   {
@@ -30,8 +36,11 @@ const rootRoutes = [
     name: "Directory",
     Icon: AddressBookTabsIcon,
   },
+  { href: "/staffing", name: "Staffing", Icon: CircuitryIcon },
 ];
-<AddressBookTabsIcon />;
+{
+  /* <AddressBookTabsIcon />; */
+}
 
 export default function NavPane({ className }) {
   return (
@@ -44,7 +53,16 @@ export default function NavPane({ className }) {
       {rootRoutes.map((route) => (
         <NavLink key={route.href} linkData={route} />
       ))}
-      <div className="flex flex-row w-full mt-auto gap-1">
+      {/* <DatePicker /> */}
+      {/* <TextInputField type="date" />
+      <label className={textFieldStyle}>
+        <input type="text" placeholder="mm/dd/yyyy" />
+        <button>
+          <CalendarIcon />
+        </button>
+      </label> */}
+      <FeatureRequestNavLink className="mt-auto" />
+      <div className="flex flex-row w-full gap-1">
         <Link to="/signin" className="btn btn-warning font-bold rounded-r-none">
           <Exit /> Sign Out
         </Link>
