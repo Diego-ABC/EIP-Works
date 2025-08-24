84ff54db05ac008f3034545fafe95b4f59c956e8
Diego <diegobcuadros@gmail.com>
2025-08-24 12:40:56 -0400

    Remove dangerous read of invite code from frontend services

--- web/src/App/features/auth/services/checkValidStaffInvite.js
+++
@@ -1,6 +0,0 @@
-import { getDoc, getFirestore, doc } from "firebase/firestore";
-export default async function checkValidStaffInvite(invCode) {
-  const codeSnap = await getDoc(doc(getFirestore(), "staffInvites/" + invCode));
-  if (!codeSnap.exists()) return false;
-  return codeSnap.data();
-}
